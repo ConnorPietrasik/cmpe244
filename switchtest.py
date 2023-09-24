6363cb7a4187a997b4676f23856064bc03476135
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-09-24 03:16:41 -0700

    test for input

---
+++ switchtest.py
@@ -0,0 +1,18 @@
+import lgpio
+import time
+
+INPIN = 22
+
+h = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_input(h, INPIN)
+
+try:
+	while True:
+		if gpio_read(h, INPIN) > 0:
+			print("1\n")
+		else:
+			print("0\n")
+		
+except KeyboardInterrupt:
+	lgpio.gpio_free(h, INPIN)
+	lgpio.gpiochip_close(h)
