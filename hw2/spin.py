a7c445e3af7afe109051ef3e833e2c6a312dd8fd
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-28 03:48:51 -0700

    added hw2 stuff

---
+++ hw2/spin.py
@@ -0,0 +1,28 @@
+import lgpio
+import sys
+#Connor Pietrasik 015126007
+
+PWM_OUT = 18
+
+#f_pwm expected as first argument, 1-10000
+f_pwm = 10000
+if (len(sys.argv) > 1):
+    f_pwm = int(sys.argv[1])
+	
+#duty_cycle expected as second argument, 0-100 int for percentage
+duty_cycle = 90
+if (len(sys.argv) > 2):
+    f_pwm = int(sys.argv[2])
+
+h = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_output(h, PWM_OUT)
+
+try:
+    while True:
+        lgpio.tx_pwm(h, 0, f_pwm, duty_cycle)
+        duty_cycle = input()
+
+except KeyboardInterrupt:
+	lgpio.tx_pwm(h, 0, 0)
+	lgpio.gpio_free(h, PWM_OUT)
+	lgpio.gpiochip_close(h)
