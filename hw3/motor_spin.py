b2fd546b6cb544e9ed7043a2391f1f6ac5f398c7
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-01 04:20:49 -0700

    added step_max setting

--- hw3/motor_spin.py
+++ hw3/motor_spin.py
@@ -2,8 +2,11 @@ import lgpio
 import time
 #Connor Pietrasik 015126007
 
-def spin_motor(clockwise = True, spin_time = 5, delay = 0.004, con0 = 17, con1 = 27, con2 = 22, con3 = 23):
-    step_max = int(spin_time / delay) + 1
+#step_max overrides spin_time if set, otherwise calculated based on spin_time and delay
+def spin_motor(clockwise = True, spin_time = 5, delay = 0.004, step_max = None, con0 = 17, con1 = 27, con2 = 22, con3 = 23):
+    #half-step, so step_max = 4096 for full rotation
+    if step_max is None:
+        step_max = int(spin_time / delay) + 1
     steps = [
         0b1001,
         0b1000,
