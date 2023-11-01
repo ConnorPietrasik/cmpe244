2b47bf36706fb0d2120a716703bbcbbcf032e2a0
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-01 02:18:11 -0700

    removed old way

--- hw3/motor_spin.py
+++ hw3/motor_spin.py
@@ -14,18 +14,6 @@ clockwise = True
 
 pins = [OUT1, OUT2, OUT3, OUT4]
 
-#Half-step sequence
-# steps = [
-#     [1,0,0,1],
-#     [1,0,0,0],
-#     [1,1,0,0],
-#     [0,1,0,0],
-#     [0,1,1,0],
-#     [0,0,1,0],
-#     [0,0,1,1],
-#     [0,0,0,1]
-# ]
-
 steps = [
     0b1001,
     0b1000,
@@ -55,25 +43,3 @@ except KeyboardInterrupt:
      cleanup()
 
 cleanup()
-
-
-#Can't use GPIO group because that sets them all the same, unfortunately
-# for pin in pins:
-#     lgpio.gpio_claim_output(h, pin)
-
-# def cleanup():
-#     for pin in pins:
-#         lgpio.gpio_write(h, pin, 0)
-#         lgpio.gpio_free(h, pin)
-#     lgpio.gpiochip_close(h)
-
-# try:
-#     step = 0
-#     for i in range(step_max):
-#         lgpio.group_write(h, OUT1, steps[step])
-#         step = (step - 1) % 8 if clockwise else (step + 1) % 8
-#         time.sleep(delay)
-# except KeyboardInterrupt:
-#      cleanup()
-
-# cleanup()
