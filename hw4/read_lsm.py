34e7b57f99e3fa0e08f0f07c02e1048ab8aaaeac
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-29 01:38:20 -0800

    added registers

--- hw4/read_lsm.py
+++ hw4/read_lsm.py
@@ -1,11 +1,26 @@
 import lgpio
-import time
+from time import sleep
 #Connor Pietrasik 015126007
 
+ADDRESS_MAG = 0x1E
+ADDRESS_ACCEL = 0x19
+
+REG_WHO_M = 0x4F
+
+REG_MAG_X_H = 0x69
+REG_MAG_Y_H = 0x6B
+REG_MAG_Z_H = 0x6D
+
 h = lgpio.i2c_open(1, 0x1E)
+
+
 while True:
     try:
-        print(lgpio.i2c_read_byte_data(h, 0))
+        x = lgpio.i2c_read_byte_data(h, REG_MAG_X_H)
+        y = lgpio.i2c_read_byte_data(h, REG_MAG_Y_H)
+        z = lgpio.i2c_read_byte_data(h, REG_MAG_Z_H)
+        print(f"X: { x }\tY: { y }\tZ: { z }")
+        sleep(0.5)
     except KeyboardInterrupt:
         lgpio.i2c_close(h)
         exit(1)
